c5041054fdb26d02b6d92f3fa25e0d0ec89d8ac1
Ross <31824737+Sorub4@users.noreply.github.com>
2018-06-15 11:17:41 -0500

    Upgrade to winston 3.0.0

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const util = require('util');
-const winston = require('winston');
+const Transport = require('winston-transport');
 const moment = require('moment');
 const _ = require('lodash');
 const elasticsearch = require('elasticsearch');
@@ -9,118 +8,127 @@ const elasticsearch = require('elasticsearch');
 const defaultTransformer = require('./transformer');
 const BulkWriter = require('./bulk_writer');
 
-/**
- * Constructor
- */
-const Elasticsearch = function Elasticsearch(options) {
-  this.options = options || {};
-  if (!options.timestamp) {
-    this.options.timestamp = function timestamp() { return new Date().toISOString(); };
-  }
-  // Enforce context
-  if (!(this instanceof Elasticsearch)) {
-    return new Elasticsearch(options);
+//
+// Inherit from `winston-transport` so you can take advantage
+// of the base functionality and `.exceptions.handle()`.
+//
+module.exports = class ElasticTransport extends Transport {
+  constructor(opts) {
+    super(opts);
+
+    //
+    // Consume any custom options here. e.g.:
+    // - Connection information for databases
+    // - Authentication information for APIs (e.g. loggly, papertrail,
+    //   logentries, etc.).
+    //
+
+    this.options = opts || {};
+    if (!opts.timestamp) {
+      this.options.timestamp = function timestamp() {
+        return new Date().toISOString();
+      };
+    }
+
+    // Enforce context
+    if (!(this instanceof ElasticTransport)) {
+      return new ElasticTransport(this.options);
+    }
+
+    // Bind to instance of ElasticTransport for use of this
+    // - Mocha discourages arrow functions and will throw if we attempt to use
+    // - See https://github.com/airbnb/javascript/issues/1379
+    this.initELasticClient = this.initELasticClient.bind(this);
+    this.initBulkWriter = this.initBulkWriter.bind(this);
+
+    // Initialize Client and Bulk Writer
+    this.initELasticClient();
+    this.initBulkWriter();
   }
 
-  // Set defaults
-  const defaults = {
-    level: 'info',
-    index: null,
-    indexPrefix: 'logs',
-    indexSuffixPattern: 'YYYY.MM.DD',
-    messageType: 'log',
-    transformer: defaultTransformer,
-    ensureMappingTemplate: true,
-    flushInterval: 2000,
-    waitForActiveShards: 1,
-    handleExceptions: false,
-    pipeline: null
-  };
-  _.defaults(options, defaults);
-  winston.Transport.call(this, options);
-
-  // Use given client or create one
-  if (options.client) {
-    this.client = options.client;
-  } else {
-    const defaultClientOpts = {
-      clientOpts: {
-        log: [
-          {
-            type: 'console',
-            level: 'error',
-          }
-        ]
-      }
+  // initialize elastic client with options
+  initELasticClient() {
+    // Set defaults
+    const defaults = {
+      level: 'info',
+      index: null,
+      indexPrefix: 'logs',
+      indexSuffixPattern: 'YYYY.MM.DD',
+      messageType: 'log',
+      transformer: defaultTransformer,
+      ensureMappingTemplate: true,
+      flushInterval: 2000,
+      waitForActiveShards: 1,
+      handleExceptions: false,
+      pipeline: null
     };
-    _.defaults(options, defaultClientOpts);
+    _.defaults(this.options, defaults);
 
-    // Create a new ES client
-    // http://localhost:9200 is the default of the client already
-    this.client = new elasticsearch.Client(this.options.clientOpts);
+    // Use given client or create one
+    if (this.options.client) {
+      this.client = this.options.client;
+    } else {
+      const defaultClientOpts = {
+        clientOpts: {
+          log: [{
+            type: 'console',
+            level: 'error'
+          }]
+        }
+      };
+      _.defaults(this.options, defaultClientOpts);
+
+      // Create a new ES client
+      // http://localhost:9200 is the default of the client already
+      this.client = new elasticsearch.Client(this.options.clientOpts);
+    }
   }
 
-  const bulkWriterOptions = {
-    interval: options.flushInterval,
-    waitForActiveShards: options.waitForActiveShards,
-    pipeline: options.pipeline,
-    ensureMappingTemplate: options.ensureMappingTemplate,
-    mappingTemplate: options.mappingTemplate,
-    indexPrefix: options.indexPrefix
-  };
+  // initialize builk writer with options
+  initBulkWriter() {
+    const bulkWriterOptions = {
+      interval: this.options.flushInterval,
+      waitForActiveShards: this.options.waitForActiveShards,
+      pipeline: this.options.pipeline,
+      ensureMappingTemplate: this.options.ensureMappingTemplate,
+      mappingTemplate: this.options.mappingTemplate,
+      indexPrefix: this.options.indexPrefix
+    };
 
-  this.bulkWriter = new BulkWriter(
-    this.client,
-    bulkWriterOptions
-  );
-  this.bulkWriter.start();
-
-  return this;
-};
-
-util.inherits(Elasticsearch, winston.Transport);
-
-Elasticsearch.prototype.name = 'elasticsearch';
-
-/**
- * log() method
- */
-Elasticsearch.prototype.log = function log(level, message, meta, callback) {
-  const logData = {
-    message,
-    level,
-    meta,
-    timestamp: this.options.timestamp()
-  };
-  const entry = this.options.transformer(logData);
-
-  this.bulkWriter.append(
-    this.getIndexName(this.options),
-    this.options.messageType,
-    entry
-  );
-
-  callback(); // write is deferred, so no room for errors here :)
-};
-
-Elasticsearch.prototype.getIndexName = function getIndexName(options) {
-  let indexName = options.index;
-  if (indexName === null) {
-    const now = moment();
-    const dateString = now.format(options.indexSuffixPattern);
-    indexName = options.indexPrefix + '-' + dateString;
+    this.bulkWriter = new BulkWriter(
+      this.client,
+      bulkWriterOptions
+    );
+    this.bulkWriter.start();
   }
-  return indexName;
+
+  // Get index name from options
+  getIndexName() {
+    let indexName = this.options.index;
+    if (indexName === null) {
+      const now = moment();
+      const dateString = now.format(this.options.indexSuffixPattern);
+      indexName = this.options.indexPrefix + '-' + dateString;
+    }
+    return indexName;
+  }
+
+  // Perform the writing to the remote service
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info);
+    });
+
+    this.bulkWriter.append(
+      this.getIndexName(this.options),
+      this.options.messageType,
+      this.options.transformer({
+        ...info,
+        timestamp: this.options.timestamp()
+      })
+    );
+
+    callback(); // write is deferred, so no room for errors here :)
+  }
 };
 
-Elasticsearch.prototype.search = function search(q) {
-  const index = this.getIndexName(this.options);
-  const query = {
-    index,
-    q
-  };
-  return this.client.search(query);
-};
-
-winston.transports.Elasticsearch = Elasticsearch;
-module.exports = Elasticsearch;
